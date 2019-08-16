066b41aab1fcd294f616453e9820d20a6a87f2ac
demonkaen <49168668+demonkaen@users.noreply.github.com>
2019-08-16 11:38:59 -0400

    Add files via upload

---
+++ setDataDocker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker exec -i deployQA rm -rf /var/www/html/*
+docker cp app.tar deployQA:/var/www/html/
+docker exec -i deployQA tar -xvf /var/www/html/app.tar -C /var/www/html/
+docker exec -i deployQA chmod 755 -R /var/www/html/
+docker exec -i deployQA chown www-data:www-data -R /var/www/html/
+docker exec -i deployQA mysql -u root -h localhost <schema.sql 
